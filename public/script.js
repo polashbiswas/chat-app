c95b2379c98ab42a0baec222a49340c35b330f7d
Polash Biswas <polashbiswasit19@gmail.com>
2023-04-22 00:18:17 +0530

    complete send and recived msg

--- public/script.js
+++ public/script.js
@@ -1,4 +1,7 @@
+const socket = io();
 let username = "";
+
+
 
 document.getElementById("join-btn").addEventListener("click", (event) => {
     event.preventDefault();
@@ -7,6 +10,8 @@ document.getElementById("join-btn").addEventListener("click", (event) => {
         document.querySelector(".form-username").style.display = "none";
         document.querySelector(".chatroom-container").style.display = "block";
         document.querySelector(".chatroom-header").innerText = `Chatroom - ${username}`;
+
+        socket.emit("username enter", username);
     } else {
         alert("Username cannot be empty");
     }
@@ -15,11 +20,44 @@ document.getElementById("join-btn").addEventListener("click", (event) => {
 
 document.getElementById("send-btn").addEventListener("click", (event) => {
     event.preventDefault();
-    var msg = document.getElementById("message-input").value;
-    var msgDiv = document.createElement("div");
-    msgDiv.innerText = msg;
-    msgDiv.setAttribute("class", "message sent");
-    document.querySelector("#messages-container").appendChild(msgDiv);
+    //inform socket.io
+    const data = {
+        username: username,
+        message: document.getElementById("message-input").value
+    };
+    //emit the message to the watchman -> give message to watchman
+    socket.emit("chat message", data);
+    //communicate with the watchman that a msg is sent
+    addMessage(data, true);
 });
+
+
+//recive user entered
+socket.on("username enter", (data) => {
+    if (data !== username) {
+        var msgDiv = document.createElement("div");
+        msgDiv.innerText = `${data} has entered!`;
+        document.querySelector("#messages-container").appendChild(msgDiv);
+    }
+});
+
+//recive message
+socket.on("chat message", (data) => {
+    if (data.username !== username) {
+        addMessage(data, false);
+    }
+});
+
+function addMessage(data, flag) {
+    var msgDiv = document.createElement("div");
+    msgDiv.innerText = `${data.username}: ${data.message}`;
+    if (flag) {
+        msgDiv.setAttribute("class", "message sent");
+    } else {
+        msgDiv.setAttribute("class", "message recived");
+    }
+
+    document.querySelector("#messages-container").appendChild(msgDiv);
+}
 
 //function if some sender sends the message, receive that message and append that child
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('User connected:', socket.id);
+
+    socket.on("chat message", (data) => {
+        io.emit("chat message", data);
+        // console.log("Message is being set to every one", data.message);
+    });
+
+    socket.on("username enter", (data) => {
+        io.emit("username enter", data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
